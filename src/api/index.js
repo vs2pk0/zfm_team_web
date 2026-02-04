0ef9ccbc31101ea6b8d222a64c68c9b00f86fd61
DaLong <904239787@qq.com>
2026-02-04 15:21:49 +0800

    Configure production baseURL and update API config

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.PROD ? 'https://zmf.sb996.cn/api' : '/api',
     timeout: 10000
 });
 
